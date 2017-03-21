d8e89056563dde9889b33b505fa4f6e9e74d847a
Ashley <ash20solomon@gmail.com>
2017-03-21 18:39:38 +0900

    subdomain priobs

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,12 @@
 class PagesController < ApplicationController
+
+
   def about
+
   end
 
   def contact
+
   end
+
 end
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,9 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_artcartridge_session'
+#Rails.application.config.session_store :cookie_store, key: '_artcartridge_session'
+
+
+Rails.application.config.session_store :cookie_store, key: '_artcartridge_session', domain: {
+  production: '.artcartridge.com',
+  development: '.localhost'
+}.fetch(Rails.env.to_sym, :all)
