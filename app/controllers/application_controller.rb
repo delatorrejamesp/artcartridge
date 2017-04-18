d856b8a67cef9c8883cbfa6d9e2ea19cfbfdbca6
Ashley <ash20solomon@gmail.com>
2017-04-18 16:01:51 +0900

    no www error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,16 +6,9 @@ class ApplicationController < ActionController::Base
 
   def after_sign_out_path_for(resource_or_scope)
       logger.info "params: #{params[:page_type]}"
-      #page_type
       return unauthenticated_root_url(subdomain: "www") if params[:page_type] == "0" && Rails.env.production?
       return unauthenticated_root_url if params[:page_type] == "0" && !Rails.env.production?
       return professional_page_url if params[:page_type]== "1"
-      #return unauthenticated_root_path(subdomain: false)  if params[:page_type] == 1
-      #unauthenticated_root_path(subdomain: false)
-      # subdomain=request.subdomain.to_s.gsub("www.", "")
-      # logger.info "subdomain : #{subdomain}"
-      #@user=User.friendly.find(subdomain)
-
   end
 
   private
